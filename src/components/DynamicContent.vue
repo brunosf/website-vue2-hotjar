aa7ce7ea7bec83ecaf5cfff66fcf25f536739481
Bruno Ferreira <bruno.ferreira@juntossomosmais.com.br>
2022-08-19 16:49:21 -0300

    first commit

---
+++ src/components/DynamicContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <fragment>
+    <div v-if="loading">
+      <p>loading...</p>
+    </div>
+    <slot v-else></slot>
+  </fragment>
+</template>
+
+<script>
+  import { Fragment } from "vue-frag";
+
+  export default {
+    components: {
+      Fragment
+    },
+    data() {
+      return {
+        loading: true,
+      }
+    },
+    mounted() {
+      this.fakeRequest()
+    },
+    methods: {
+      fakeRequest() {
+        new Promise((resolve) => {
+          setTimeout(() => {
+            this.loading = false
+            resolve()
+          }, 3000);
+        })
+      }
+    }
+  }
+</script>
---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>dashboard</h1>
+    <DynamicContent>
+      <em>Slot do component DynamicContent</em>
+      <hello-world msg="Teste Hotjar + Fragment in Vue2" />
+    </DynamicContent>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+import DynamicContent from '../components/DynamicContent.vue';
+export default {
+  name: 'Dashboard',
+  components: { DynamicContent, HelloWorld }
+}
+</script>
